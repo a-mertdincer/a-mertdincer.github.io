451e3bcb135637ace935314a5f1d7e7cfeab6040
Mert <a.mertdincr@gmail.com>
2023-12-31 16:44:53 +0300

    first commit

---
+++ js/commands.js
@@ -0,0 +1,80 @@
+var blog = "https://lsociety.net"
+var twitter = "https://www.x.com/jamiroquaei/";
+var password = "d0tslash";
+var linkedin = "https://www.linkedin.com/in/a-mertdincer/";
+var github = "https://github.com/a-mertdincer/";
+var email = 'mailto:a.mertdincr@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Mert!👋",
+  "My name is Mert, a 23-year-old computer engineering student passionate about cybersecurity.",
+  "For the past two years, I've been actively involved in the cybersecurity sector, honing my skills through various bootcamps",
+  "and tackling challenges on platforms like TryHackMe and Hack The Box. Alongside my academic pursuits, I co-founded LSociety.net,",
+  "a blog where I contribute as a writer. At LSociety.net, we focus on delivering insightful content about cybersecurity, including news,",
+  "articles, and machine-solving solutions. Currently working on my graduation project — a cutting-edge cybersecurity training platform —",
+  "I find great joy in sharing my knowledge and experiences on both fronts. Join me on this exciting journey as we explore the ever-evolving landscapes",
+  "of computer science, software engineering, and cybersecurity!",
+  "<br>"
+
+];
+
+whoami = [
+  "<br>",
+  "“Who am I? I mean, where am I?”",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">x/jamiroquaei' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/a-mertdincer' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/a-mertdincer' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  'Our Blog Page           <a href="' + blog + '" target="_blank">LSociety' + '</a>',
+  'My Github Repos         <a href="' + github + '" target="_blank">github/a-mertdincer' + "</a>",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Mert?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">blog</span>           View my blog page',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Feel free to email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  " .----------------.  .----------------.",
+  "| .--------------. || .--------------.| ",
+  "| |              | || |        __    || ",
+  "| |              | || |       / /    || ",
+  "| |              | || |      / /     || ",
+  "| |              | || |     / /      || ",
+  "| |      _       | || |    / /       || ",
+  "| |     (_)      | || |   /_/        || ",
+  "| |              | || |              || ",
+  "| '--------------' || '--------------'| ",
+  "'----------------'  '----------------'",
+  
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
